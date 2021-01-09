04d22d20f1456c6a6940d2f126467d27afb29152
phoboz <leputa6@hotmail.com>
2021-01-09 21:13:59 +0100

    Added support for user defined tile types

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tile_types.h"
 #include "Tmx.h"
 
 void write_word(short value, FILE *fp) {
   fputc((value & 0xff00) >> 8, fp);
   fputc(value & 0x00ff, fp);
+}
+
+char get_tile_type(const char *str)
+{
+  char result = TILE_TYPE_NONE;
+
+  if (strcmp(str, "floor") == 0)
+    result = TILE_TYPE_FLOOR;
+  else if (strcmp(str, "rock") == 0)
+    result = TILE_TYPE_ROCK;
+  else if (strcmp(str, "metal") == 0)
+    result = TILE_TYPE_METAL;
+  else if (strcmp(str, "special_1") == 0)
+    result = TILE_TYPE_SPECIAL_1;
+  else if (strcmp(str, "special_2") == 0)
+    result = TILE_TYPE_SPECIAL_2;
+  else if (strcmp(str, "overlay") == 0)
+    result = TILE_TYPE_OVERLAY;
+
+  return result;
 }
 
 int main(int argc, char **argv) {
@@ -31,15 +54,43 @@ printf("parsed map...\n");
     return 1;
   }
 
-  const Tmx::Layer *layer = map->GetLayer(0);
-  if (!layer) {
-    printf("error: layer zero does not exist\n");
-    return 1;
-  }
-
   FILE *fp = fopen(argv[2], "wb");
   if (fp == NULL) {
     printf("error: unable to create file %s\n", argv[2]);
+    return 1;
+  }
+
+  const Tmx::Tileset *tileset = map->GetTileset(0);
+  if (!tileset) {
+    printf("error - no tileset exist\n");
+    return 1;
+  }
+
+  std::vector<Tmx::Tile*> tiles = tileset->GetTiles();
+  unsigned char num_tiles = tiles.size();
+
+  printf("Number of tiles: %d\n", num_tiles);
+  fputc(num_tiles, fp);
+
+  for (int i = 0; i < (int) num_tiles; i++) {
+    Tmx::Tile *tile = tiles[i];
+    const Tmx::PropertySet prop = tile->GetProperties();
+    std::string value = prop.GetLiteralProperty(std::string("type"));
+    char type = get_tile_type(value.c_str());
+    if (type == TILE_TYPE_OVERLAY) {
+      std::string value = prop.GetLiteralProperty(std::string("bg_tile"));
+      char bg = (char) atoi(value.c_str());
+      bg <<= 1;
+      type |= bg;
+    }
+
+    printf("tile: %d %s(0x%x)\n", tile->GetId(), value.c_str(), type & 0xFF);
+    fputc(type, fp);
+  }
+
+  const Tmx::Layer *layer = map->GetLayer(0);
+  if (!layer) {
+    printf("error: layer zero does not exist\n");
     return 1;
   }
 
---
+++ tile_types.h
@@ -0,0 +1,13 @@
+#ifndef _TILE_TYPES_H
+#define _TILE_TYPES_H
+
+#define TILE_TYPE_NONE      0x00
+#define TILE_TYPE_OVERLAY   0x01
+#define TILE_TYPE_FLOOR     0x02
+#define TILE_TYPE_ROCK      0x10
+#define TILE_TYPE_METAL     0x12
+#define TILE_TYPE_SPECIAL_1 0x14
+#define TILE_TYPE_SPECIAL_2 0x16
+
+#endif
+
